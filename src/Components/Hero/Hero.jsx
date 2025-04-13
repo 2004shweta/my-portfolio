42d75d2a2c83138a7cde541e6d4e5c0c257f6537
2004shweta <156895477+2004shweta@users.noreply.github.com>
2025-04-13 22:55:14 +0530

    Adding navbar

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+    <img src={profile_img} alt="" />
+    <h1><span>I'am Shweta </span> hello</h1>
+    <p> full stack web developer</p>
+    <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Hero
