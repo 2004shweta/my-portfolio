5c20461dc3e18d11b4bacefc8b1c0267ea74f03b
2004shweta <156895477+2004shweta@users.noreply.github.com>
2025-04-28 20:29:12 +0530

    upadting cv link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -71,7 +71,7 @@ const profile = new DeveloperProfile();`;
   const handleCvSelect = (cvType) => {
     const cvLinks = {
       specialized: "https://drive.google.com/file/d/1vXhTLUvBFzy2_lNeCkD2BiReMnAU_694/view?usp=sharing",
-      general: "https://drive.google.com/file/d/1yZiNUbdoGBP8aSgUCaI6P91mNMmM0F5i/view?usp=sharing" // Replace with your general CV link
+      general: "https://drive.google.com/file/d/1AtNSC6p-pm8eDACn2Y-jfAus_F1U8TqN/view?usp=sharing" // Replace with your general CV link
     };
     window.open(cvLinks[cvType], "_blank");
     setShowCvDropdown(false);
